5c3a7c51f1beaae7204f91634703512cea9fb3d6
Heqian Liu <lhq@bu.edu>
2022-01-15 17:26:49 -0800

    fix cors

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
+var cors = require('cors')
 const app = express();
+app.use(cors())
 const port = 2000;
 const post = require("./post");
 const bodyParser = require('body-parser');
@@ -17,21 +19,6 @@ const connectionpOptions = {
 const connectionUri = `mongodb://chippin:jwjIjwi5v2bckWjyI31ntKllvLSwc3wk9mWKF1AkXpwXlAvC32Gh1jeSJnK7JCdj8LwG41GvjQa9jC2S5IQs0w==@chippin.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@chippin@`;
 mongoose.connect(connectionUri, connectionpOptions);
 const PostModel = mongoose.model('posts', post.postsSchema);
-
-app.use(function (req, res, next) {
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    // Pass to next layer of middleware
-    next();
-});
-
 
 // Get post
 app.get('/:id', (req, res) => {
